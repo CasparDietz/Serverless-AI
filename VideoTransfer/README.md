a3a929cfb2e63998aeb78b7661894cea014fe4ff
CasparDietz <35566632+CasparDietz@users.noreply.github.com>
2022-04-28 11:03:56 +0200

    Video Transfer

--- VideoTransfer/README.md
+++ VideoTransfer/README.md
@@ -1,1 +1,4 @@
+This folder contains the video reciever and sender.
+To run the two, first run reciever.py in one terminal window and the run sender.py in another terminal window.
 
+Frames are saved on the reciever side in the folder frames.
---
+++ VideoTransfer/sender.py
@@ -0,0 +1,45 @@
+import cv2
+import numpy as np
+import socket
+import sys
+import pickle
+import struct
+
+cap=cv2.VideoCapture('videos/sample2.mp4')
+#cap=cv2.VideoCapture('videos/sample-video.mp4')
+clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+clientsocket.connect(('localhost',8089))
+
+
+if (cap.isOpened() == False):
+	print("Error opening the video file")
+# Read fps and frame count
+else:
+	# Get frame rate information
+	fps = cap.get(5)
+	print('Frames per second : ', fps,'FPS')
+
+	# Get frame count
+	frame_count = cap.get(7)
+	print('Frame count : ', frame_count)
+    
+while cap.isOpened():
+    ret,frame=cap.read()
+    if ret == True:
+        # Serialize frame
+        data = pickle.dumps(frame)
+
+        # Send message length first
+        message_size = struct.pack("L", len(data))
+
+        # Then data
+        clientsocket.sendall(message_size + data)
+        print('Frame sent')
+    else:
+        # When the last frame is reached, send a message to the server to close the connection
+        data = pickle.dumps(int(0))
+        message_size = struct.pack("L", len(data))
+        clientsocket.sendall(message_size + data)
+        break
+
+cap.release()
